38fe61c1665bf026f971117c4f25633cb8d118ed
phuongnd2 <phuongnd2@fsoft.com.vn>
2017-03-09 14:38:52 +0700

    Add normalization

--- OCRMock/src/main/java/com/fsoft/cme/ocr/controller/FileUploadController.java
+++ OCRMock/src/main/java/com/fsoft/cme/ocr/controller/FileUploadController.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -22,12 +23,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.fsoft.cme.ocr.utils.StringUtils;
+
 @Controller
 public class FileUploadController {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(FileUploadController.class);
 	
 	public static final String ROOT = "upload-dir";
+	
+	@Value("${image.url}")
+	private String imageUrl;
 	
 	@RequestMapping(method = RequestMethod.POST, value = "/uploadFile")
 	@ResponseBody
@@ -36,10 +42,27 @@ public class FileUploadController {
 
 		try {
 			if (!file.isEmpty()) {
+				String saveFileName = start + "_" + StringUtils.normalizeFileName(file.getOriginalFilename());
+				Files.copy(file.getInputStream(), Paths.get(ROOT, saveFileName));
+				return new URLResponse(imageUrl + saveFileName);
+			}
+		}  catch (Exception e) {
+			LOGGER.error("System error ", e);
+		}
+		LOGGER.info("Upload time time: " + (System.currentTimeMillis() - start) + " ms.");
+		return null;
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/uploadFile")
+	@ResponseBody
+	public URLResponse handleJsonUpload(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
+		long start = System.currentTimeMillis();
+
+		try {
+			if (!file.isEmpty()) {
 				String saveFileName = start + "_" + file.getOriginalFilename();
 				Files.copy(file.getInputStream(), Paths.get(ROOT, saveFileName));
-				String url = "https://dev-freq-cms-proxy.freq.us:" + request.getLocalPort() + "/freq-img/image?img=" + saveFileName;
-				return new URLResponse(url);
+				return new URLResponse(imageUrl + saveFileName);
 			}
 		}  catch (Exception e) {
 			LOGGER.error("System error ", e);
@@ -78,5 +101,4 @@ public class FileUploadController {
 			return url;
 		}
 	}
-
 }
---
+++ OCRMock/src/main/java/com/fsoft/cme/ocr/utils/StringUtils.java
@@ -0,0 +1,15 @@
+package com.fsoft.cme.ocr.utils;
+
+public class StringUtils {
+
+	public static String normalizeFileName(String fileName){
+		if (fileName != null && fileName.length() > 0 && fileName.lastIndexOf(".") > 0){
+			String extension = fileName.substring(fileName.lastIndexOf("."));
+			String name = fileName.substring(0, fileName.lastIndexOf("."));
+			name = name.replaceAll("[^a-zA-Z0-9]+","");
+			return name + extension;
+		}
+		return "";
+	}
+	
+}
